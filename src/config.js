4a48968be568a68aeb1c9c33674e32f2179bf2eb
Himanshu <himanshujangra0633>
2025-10-10 10:22:39 +0530

    dsfds

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // const API_URL = 'https://bj5x3fl4-6000.inc1.devtunnels.ms/api'
-const API_URL = 'https://backendruno.ciisnetwork.in/api'
+const API_URL = 'https://backendcds.ciisnetwork.in/api'
 // const API_URL = 'http://147.93.106.84:6000/api'
 export default API_URL;
