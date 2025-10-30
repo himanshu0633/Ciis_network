34ef54591c882a891345c876cc6703ca84488aa7
Himanshu <himanshu@Himanshus-MacBook-Air.local>
2025-10-30 16:30:00 +0530

    done h

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'http://localhost:3000',
         // target: 'http://147.93.106.84:6000',
         changeOrigin: true,
         secure: false,
