05a68747606204ec4a60760684efcdc1a313329f
Himanshu <himanshu@Himanshus-MacBook-Air.local>
2025-12-12 18:40:34 +0530

    sad

--- src/main.jsx
+++ src/main.jsx
@@ -4,21 +4,19 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App.jsx';
 
-// MUI v4 FIX: Required for stable classnames in production
-import { StylesProvider } from '@material-ui/core/styles';
-import generateClassName from './utils/generateClassName';
+
 
 // Contexts
 import { AuthProvider } from './context/AuthContext';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <StylesProvider generateClassName={generateClassName}>
+   
       <AuthProvider>
         <BrowserRouter>
           <App />
         </BrowserRouter>
       </AuthProvider>
-    </StylesProvider>
+    
   </StrictMode>
 );
