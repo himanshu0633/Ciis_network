24cea917dfdfc36f0f79b71efdcf1563c55ab8c2
Mohit <ms8570097@gmail.com>
2026-02-06 09:32:30 +0530

    push

--- src/config.js
+++ src/config.js
@@ -1,6 +1,8 @@
-export const API_URL = 'https://backendcds.ciisnetwork.in/api'
-export const API_URL_IMG = 'https://backendcds.ciisnetwork.in/'
+// export const API_URL = 'https://backendcds.ciisnetwork.in/api'
+// export const API_URL_IMG = 'https://backendcds.ciisnetwork.in/'
 // export const API_URL = 'http://localhost:3000/api'
 // export const API_URL_IMG = 'http://localhost:3000/'
+export const API_URL = 'https://j449fxt7-3000.inc1.devtunnels.ms/api'
+export const API_URL_IMG = 'https://j449fxt7-3000.inc1.devtunnels.ms/'
 
 export default API_URL;
